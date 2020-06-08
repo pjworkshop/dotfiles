269015e910116e5600a497a4f9c8c37e5df14353
pjworkshop <pjworkshop@protonmail.com>
2020-06-08 11:40:26 -0500

    moved to chromium

--- .bashrc
+++ .bashrc
@@ -7,6 +7,8 @@
 
 force_color_prompt=yes
 
-alias ls="ls -a --color=auto"
-PS1='[\u@\h \W]\$ '
+PS1="\[\e[33m\][\[\e[m\]\[\e[35m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]:\[\e[32m\]\w\[\e[m\]\[\e[33m\]]\[\e[m\]\[\e[32m\]\\$\[\e[m\] "
 
+alias ls='ls -a --color'
+alias nf='neofetch'
+alias vim='nvim'
--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -62,6 +65,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *inclight[] = { "brightnessctl", "set",            "+10%",  NULL };
+static const char *declight[] = { "brightnessctl", "set",             "10%-",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -98,6 +106,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{ 0,                    XF86XK_MonBrightnessUp,      spawn, {.v = inclight } },
+	{ 0,                    XF86XK_MonBrightnessDown,      spawn, {.v = declight } },
+
 };
 
 /* button definitions */
